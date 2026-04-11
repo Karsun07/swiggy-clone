631d8b5387821c35583079734a585ac70b25810e
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-04-11 11:00:41 +0530

    scroller added on shop groceries

--- src/components/Grocerycard.js
+++ src/components/Grocerycard.js
@@ -1,8 +1,8 @@
 export default function Grocerycard({value}){
     return (
         <div className="text-center">
-            <a href={value?.action?.link}><img className="w-36 h-45" src={"https://media-assets.swiggy.com/swiggy/image/upload/"+value.imageId}></img></a>
-            <label className="w-36 h-12 object-cover font-medium  ">{value?.action?.text}</label>
+            <a href={value?.action?.link}><img className="w-40 aspect-square object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/"+value.imageId}></img></a>
+            <label className="block w-40 text-center font-medium ">{value?.action?.text}</label>
         </div>
     )
 }
--- src/components/Groceryoptions.js
+++ src/components/Groceryoptions.js
@@ -4,7 +4,7 @@ export default function Groceryoptions() {
     return (
         <div>
             <h1 className="text-2xl font-bold relative left-40 mt-20"> Shop groceries on Instamart</h1>
-            <div className="max-w-[80%]  flex flex-wrap gap-8 container mx-auto mt-5 ">
+            <div className="max-w-[80%]  flex flex-nowrap overflow-x-auto gap-8 container mx-auto mt-5 ">
                 {
                     GroceryGridCards.map((value) => <Grocerycard key={value.id} value={value} />)
                 }
