9219357063692b06c2c64d33f68d5da9d566f4fa
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-04-12 12:43:18 +0530

    got arrays of restaurant and whats on mind

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 
 export default function Restaurants(){
-    const [restData,setRestData]=useState({});
+    const [restData,setRestData]=useState([]);
+    const [mindData,setMindData]=useState([]);
     useEffect(()=>{
         async function fetchData() {
             const proxyServer="https://cors-anywhere.herokuapp.com/";
             const swiggyAPI="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
             const response=await fetch(proxyServer+swiggyAPI);
             const data=await response.json();
-            setRestData(data);   
+            setRestData(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);   
+            setMindData(data.data.cards[0].card.card.imageGridCards.info);
         }
         fetchData();
     },[])
     console.log(restData);
+    console.log(mindData);
 }
